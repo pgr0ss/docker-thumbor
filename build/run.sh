ab72838aadcd64a6ac87f389049d9453fca9a7e6
Paul Gross <pgross@gmail.com>
2015-10-06 11:00:59 -0700

    Upgrade tc_aws.

--- build/run.sh
+++ build/run.sh
@@ -35,7 +35,6 @@ chmod +x /etc/service/thumbor/run
 apt-get update -y
 apt-get install -y --no-install-recommends \
     build-essential \
-    git \
     python python-dev python-pip \
     webp libwebp-dev \
     gifsicle libgif-dev \
@@ -55,7 +54,7 @@ tar -jxvf /tmp/jpeg-archive.tar.bz2 -C /usr/bin --wildcards 'jpeg-*'
 pip install \
     remotecv graphicsmagick-engine opencv-engine j2cli \
     Pillow==2.9.0 \
-    git+https://github.com/pgr0ss/aws.git@fix_key \
+    tc_aws==1.3.2 \
     thumbor==$THUMBOR_VERSION
 
 pip install https://github.com/zanui/thumbor-plugins/archive/jpegrecompress_add_options.zip
